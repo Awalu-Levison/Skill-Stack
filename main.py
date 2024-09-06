a69c09d2fb360ffbfd8d52d7b6a81db4a942bbea
Awalu Levison <79022742+Awalu-Levison@users.noreply.github.com>
2024-09-06 06:54:18 +0200

    Addes some files

--- main.py
+++ main.py
@@ -24,11 +24,6 @@ def about():
 def contact():
     return render_template('contact.html')
 
-"""Learners dashboard"""
-@app.route("/dashboard")
-def dashboard():
-    return render_template('dashboard.html')
-
 
 if __name__ == "__main__":
     """run the application outside the development server"""
